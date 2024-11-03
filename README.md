bc59592b1639b7f788d19deb90fa87b3d3628a7f
flayen <105023608+flayen@users.noreply.github.com>
2024-11-04 00:11:52 +0800

    release: v1.0.0

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-> [!NOTE]
-> This project is a work in progress and might not work as intended
 # Flayen's Certificate Builder
 This is a simple certificate builder that uses the `Pillow` library to create certificates. It is a simple script that reads a CSV file, certificate image, and font then creates a certificate for each row in the CSV file.
 The generated certificates are saved in a paginated pdf via `PyPDF2` inside the `output` folder.
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import csv
 from PIL import Image, ImageDraw, ImageFont
-
-def read_csv(file_path):
-    with open(file_path, mode='r', newline='') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            print(row)
-
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import os
 
 def add_text_to_image(image_path, text, font_path, font_size, output_path):
     image = Image.open(image_path)
@@ -14,14 +10,40 @@ def add_text_to_image(image_path, text, font_path, font_size, output_path):
 
     font = ImageFont.truetype(font_path, font_size)
 
-    text_width, text_height = draw.textsize(text, font=font)
+    text_bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
     image_width, image_height = image.size
     x = (image_width - text_width) / 2
     y = (image_height - text_height) / 2
 
     draw.text((x, y), text, font=font, fill="black")
 
-    image.save(output_path)
+    image.save(output_path, format='PNG')
+    return output_path
+
+def save_images_to_pdf(image_paths, output_pdf_path):
+    c = canvas.Canvas(output_pdf_path, pagesize=letter)
+    for image_path in image_paths:
+        image = Image.open(image_path)
+        image_width, image_height = image.size
+        c.setPageSize((image_width, image_height))
+        c.drawImage(image_path, 0, 0, width=image_width, height=image_height)
+        c.showPage()
+    c.save()
+
+def generate_certificates(csv_path, image_template_path, font_path, font_size, output_pdf_path):
+    image_paths = []
+    os.makedirs('generated', exist_ok=True)
+    with open(csv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            name = row['name']
+            output_image_path = os.path.join('generated', f'output_{name.replace(" ", "_")}.png')
+            add_text_to_image(image_template_path, name, font_path, font_size, output_image_path)
+            image_paths.append(output_image_path)
+
+    save_images_to_pdf(image_paths, output_pdf_path)
 
 if __name__ == "__main__":
-    read_csv('assets/data.csv')
+    generate_certificates('assets/data.csv', 'assets/certificate.jpg', 'assets/font/Roboto-Regular.ttf', 50, 'generated/output_certificates.pdf')
